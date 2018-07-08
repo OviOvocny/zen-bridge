f21810f55613f89c7a38fdcd9a11e3ee272603e8
OviOvocny <werint.pp@gmail.com>
2018-07-09 00:05:15 +0200

    finish implementing Danbooru 2 (except specs)

--- src/lib/booru-types/danbooru2.ts
+++ src/lib/booru-types/danbooru2.ts
@@ -5,24 +5,204 @@ import convertRating from './../utils/rating-converter'
 /**
  * A Booru implementation for sites running Danbooru version 2
  */
-export default class Danbooru2 extends Booru {
+class Danbooru2 extends Booru {
   readonly xml = false
 
-  post (id: number): Promise<Post> {
-    return this.fetch(UriBuilder.post(id)).then(data => this.postConverter(data))
+  private genericSingle<T> (type: string, id: number): Promise<T> {
+    return this.fetch((<any>Danbooru2.UriBuilder)[type](id)).then(data => <T>(<any>Danbooru2.Converter)[type](data))
   }
 
-  posts (query: Query.Posts): Promise<Post[]> {
-    return this.fetch(UriBuilder.posts(query))
-      .then((data: Array<object>) => data.map(this.postConverter))
+  private genericQuery<T> (type: string, query: any): Promise<T[]> {
+    return this.fetch((<any>Danbooru2.UriBuilder)[`${type}s`](query))
+      .then((data: Array<object>) => <T[]>data.map((<any>Danbooru2.Converter)[type]))
+  }
+
+  post (id: number) {
+    return this.genericSingle<Post>('post', id)
+  }
+
+  posts (query: Query.Posts) {
+    return this.genericQuery<Post>('post', query)
       .then((posts: Post[]) => query.exclude ? posts.filter(post => !query.exclude!.some(tag => post.tags.all.includes(tag))) : posts)
   }
 
-  /**
-   * Create a Post from an API response
-   * @param data A post object from the API
-   */
-  private postConverter (data: any): Post {
+  artist (id: number) {
+    return this.genericSingle<Artist>('artist', id)
+  }
+
+  artists (query: Query.Artists) {
+    return this.genericQuery<Artist>('artist', query)
+  }
+
+  comment (id: number) {
+    return this.genericSingle<Comment>('comment', id)
+  }
+
+  comments (query: Query.Comments): Promise<Comment[]>
+  comments (post: Post): Promise<Post>
+  comments (query: Query.Comments | Post) {
+    if (query.hasOwnProperty('id')) {
+      // This is a Post, fill in the comments field
+      const post = <Post>query
+      return this.genericQuery<Comment>('comment', <Query.Comments>{
+        postId: post.id
+      }).then(commentArr => {
+        post.comments = commentArr
+        return post
+      })
+    } else {
+      // This is a query, get comments array
+      return this.genericQuery<Comment>('comment', query)
+    }
+  }
+
+  note (id: number) {
+    return this.genericSingle<Note>('note', id)
+  }
+
+  notes (query: Query.Notes): Promise<Note[]>
+  notes (post: Post): Promise<Post>
+  notes (query: Query.Notes | Post) {
+    if (query.hasOwnProperty('id')) {
+      // This is a Post, fill in the comments field
+      const post = <Post>query
+      return this.genericQuery<Note>('note', <Query.Notes>{
+        postId: post.id
+      }).then(noteArr => {
+        post.notes = noteArr
+        return post
+      })
+    } else {
+      // This is a query, get comments array
+      return this.genericQuery<Note>('note', query)
+    }
+  }
+
+  pool (id: number) {
+    return this.genericSingle<Pool>('pool', id)
+  }
+
+  pools (query: Query.Pools) {
+    return this.genericQuery<Pool>('pool', query)
+  }
+
+  user (id: number) {
+    return this.genericSingle<User>('user', id)
+  }
+
+  users (query: Query.Users) {
+    return this.genericQuery<User>('user', query)
+  }
+
+  wiki (id: number) {
+    return this.genericSingle<Wiki>('wiki', id)
+  }
+
+  wikis (query: Query.Wikis) {
+    return this.genericQuery<Wiki>('wiki', query)
+  }
+
+}
+
+Danbooru2.UriBuilder = {
+  post (id: number): string {
+    return `/posts/${id}.json`
+  },
+  posts (query: Query.Posts): string {
+    let queryString = ''
+    if (query.tags) {
+      let t = query.tags
+      // Danbooru limitation, probably done wrong
+      if (t.length > 2) {
+        t = t.slice(0, 2)
+      }
+      queryString = queryStringify({
+        ...query,
+        tags: t.join(' ')
+      })
+    } else {
+      queryString = queryStringify(query)
+    }
+    return '/posts.json?' + queryString
+  },
+  artist (id: number): string {
+    return `/artists/${id}.json`
+  },
+  artists (query: Query.Artists): string {
+    return '/artists.json?' + queryStringify({
+      name: query.nameMatches,
+      order: query.order
+    })
+  },
+  comment (id: number): string {
+    return `/comments/${id}.json`
+  },
+  comments (query: Query.Comments): string {
+    return '/comments.json?' + queryStringify({
+      'group_by': 'comment',
+      ...query,
+      'search[body_matches]': query.contentMatches,
+      'search[post_id]': query.postId,
+      'search[creator_id]': query.creator ? query.creator.id : undefined,
+      'search[creator_name]': query.creator ? query.creator.name : undefined
+    })
+  },
+  note (id: number): string {
+    return `/notes/${id}.json`
+  },
+  notes (query: Query.Notes): string {
+    return '/notes.json?' + queryStringify({
+      'search[body_matches]': query.contentMatches,
+      'search[post_id]': query.postId,
+      'search[creator_id]': query.creator ? query.creator.id : undefined,
+      'search[creator_name]': query.creator ? query.creator.name : undefined
+    })
+  },
+  pool (id: number): string {
+    return `/pools/${id}.json`
+  },
+  pools (query: Query.Pools): string {
+    return '/pools.json?' + queryStringify({
+      'search[name_matches]': query.nameMatches,
+      'search[order]': query.order,
+      'search[creator_id]': query.creator ? query.creator.id : undefined,
+      'search[creator_name]': query.creator ? query.creator.name : undefined
+    })
+  },
+  user (id: number): string {
+    return `/users/${id}.json`
+  },
+  users (query: Query.Users): string {
+    const transformed: any = {
+      'search[name]': query.nameMatches,
+      'search[order]': query.order
+    }
+    if (query.level) {
+      if (typeof query.level === 'number') {
+        transformed['search[level]'] = query.level
+      } else {
+        transformed['search[min_level]'] = query.level[0] || 0
+        transformed['search[max_level]'] = query.level[1] || 9001
+      }
+    }
+    return '/users.json?' + queryStringify(transformed)
+  },
+  wiki (id: number): string {
+    return `/wiki_pages/${id}.json`
+  },
+  wikis (query: Query.Wikis): string {
+    return '/wiki_pages.json?' + queryStringify({
+      'search[title]': query.title,
+      'search[body_matches]': query.contentMatches,
+      'search[order]': query.order,
+      'search[creator_id]': query.creator ? query.creator.id : undefined,
+      'search[creator_name]': query.creator ? query.creator.name : undefined
+    })
+  }
+}
+
+Danbooru2.Converter = {
+  post (data: any): Post {
     const tagTypes = ['general', 'character', 'copyright', 'artist', 'meta']
     const post: Post = {
       id: parseInt(data.id),
@@ -66,30 +246,95 @@ export default class Danbooru2 extends Booru {
       }
     })
     return post
-  }
-}
-
-class UriBuilder {
-  static post (id: number): string {
-    return `/posts/${id}.json`
-  }
-
-  static posts (query: Query.Posts): string {
-    const base = '/posts.json'
-    let queryString = ''
-    if (query.tags) {
-      let t = query.tags
-      // Danbooru limitation, probably done wrong
-      if (t.length > 2) {
-        t = t.slice(0, 2)
-      }
-      queryString = queryStringify({
-        ...query,
-        tags: t.join(' ')
-      })
-    } else {
-      queryString = queryStringify(query)
+  },
+  user (data: any): User {
+    return {
+      id: data.id,
+      name: data.name,
+      banned: data.is_banned,
+      level: data.level
     }
-    return queryString === '' ? base : base + '?' + queryString
+  },
+  comment (data: any): Comment {
+    return {
+      id: data.id,
+      createdAt: data.created_at,
+      active: !data.is_deleted,
+      creator: {
+        id: data.creator_id,
+        name: data.creator_name
+      },
+      content: data.body,
+      postId: data.post_id,
+      score: data.score
+    }
+  },
+  note (data: any): Note {
+    return {
+      id: data.id,
+      createdAt: data.created_at,
+      active: data.is_active,
+      creator: {
+        id: data.creator_id,
+        name: data.creator_name
+      },
+      content: data.body,
+      postId: data.post_id,
+      position: {
+        x: data.x,
+        y: data.y
+      },
+      dimensions: {
+        width: data.width,
+        height: data.height
+      }
+    }
+  },
+  artist (data: any): Artist {
+    return {
+      id: data.id,
+      createdAt: data.created_at,
+      active: data.is_active,
+      creator: {
+        id: data.creator_id
+      },
+      name: data.name,
+      aliases: data.other_names.split(' '),
+      group: data.group_name,
+      links: data.urls.map((obj: any) => obj.url),
+      about: data.notes
+    }
+  },
+  pool (data: any): Pool {
+    return {
+      id: data.id,
+      createdAt: data.created_at,
+      active: data.is_active,
+      creator: {
+        id: data.creator_id,
+        name: data.creator_name
+      },
+      name: data.name,
+      category: data.category,
+      postCount: data.post_count,
+      postIds: data.post_ids.split(' ').map(parseFloat),
+      description: data.description
+    }
+  },
+  wiki (data: any): Wiki {
+    return {
+      id: data.id,
+      createdAt: data.created_at,
+      active: !data.is_deleted,
+      creator: {
+        id: data.creator_id,
+        name: data.creator_name
+      },
+      title: data.title,
+      content: data.body,
+      aliases: data.other_names
+    }
   }
 }
+
+export default Danbooru2
--- src/lib/booru.ts
+++ src/lib/booru.ts
@@ -1,11 +1,42 @@
 import dataFetcher from './utils/data-fetcher'
 
+namespace BooruHelper {
+  export interface UriBuilder {
+    artist? (id: number): string
+    artists? (query: Query.Artists): string
+    comment? (id: number): string
+    comments? (query: Query.Comments): string
+    note? (id: number): string
+    notes? (query: Query.Notes): string
+    pool? (id: number): string
+    pools? (query: Query.Pools): string
+    post (id: number): string
+    posts (query: Query.Posts): string
+    user? (id: number): string
+    users? (query: Query.Users): string
+    wiki? (id: number): string
+    wikis? (query: Query.Wikis): string
+  }
+  export interface Converter {
+    artist? (data: any): Artist
+    comment? (data: any): Comment
+    note? (data: any): Note
+    pool? (data: any): Pool
+    post (data: any): Post
+    user? (data: any): User
+    wiki? (data: any): Wiki
+  }
+}
+
 /**
  * An instance of a specific booru site. 
  * Has common methods for getting data from a booru.
  * Can be used on its own or as a ZenBridge service.
  */
 export default abstract class Booru {
+  static UriBuilder: BooruHelper.UriBuilder
+  static Converter: BooruHelper.Converter
+
   /** True if this booru has a XML-based API */
   abstract readonly xml: boolean
 
@@ -34,4 +65,88 @@ export default abstract class Booru {
    * @param query Posts search query object
    */
   abstract posts (query: Query.Posts): Promise<Post[]>
+
+  /**
+   * Get a single Artist by ID
+   * @param id ID of the artist
+   */
+  artist? (id: number): Promise<Artist>
+
+  /**
+   * Get an array of Artists that match a query
+   * @param query Artists search query object
+   */
+  artists? (query: Query.Artists): Promise<Artist[]>
+
+  /**
+   * Get a single Comment by ID
+   * @param id ID of the comment
+   */
+  comment? (id: number): Promise<Comment>
+
+  /**
+   * Get an array of Comments that match a query
+   * @param query Comments search query object
+   */
+  comments? (query: Query.Comments): Promise<Comment[]>
+
+  /**
+   * Get back the Post you passed in with the comments field populated
+   * @param post The post to find comments for
+   */
+  comments? (post: Post): Promise<Post>
+
+  /**
+   * Get a single Note by ID
+   * @param id ID of the note
+   */
+  note? (id: number): Promise<Note>
+
+  /**
+   * Get an array of Notes that match a query
+   * @param query Notes search query object
+   */
+  notes? (query: Query.Notes): Promise<Note[]>
+
+  /**
+   * Get back the Post you passed in with the notes field populated
+   * @param post The post to find notes for
+   */
+  notes? (post: Post): Promise<Post>
+
+  /**
+   * Get a single Pool by ID
+   * @param id ID of the pool
+   */
+  pool? (id: number): Promise<Pool>
+
+  /**
+   * Get an array of Pools that match a query
+   * @param query Pools search query object
+   */
+  pools? (query: Query.Pools): Promise<Pool[]>
+
+  /**
+   * Get a single User by ID
+   * @param id ID of the user
+   */
+  user? (id: number): Promise<User>
+
+  /**
+   * Get an array of Users that match a query
+   * @param query Users search query object
+   */
+  users? (query: Query.Users): Promise<User[]>
+
+  /**
+   * Get a single Wiki page by ID
+   * @param id ID of the wiki page
+   */
+  wiki? (id: number): Promise<Wiki>
+
+  /**
+   * Get an array of Wikis that match a query
+   * @param query Wikis search query object
+   */
+  wikis? (query: Query.Wikis): Promise<Wiki[]>
 }
